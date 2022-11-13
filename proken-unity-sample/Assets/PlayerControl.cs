75822810efb6525a537cd7e30ff6176017360365
Nachia Vivias <piyopiyograph@gol.com>
2022-11-13 22:34:55 +0900

    install InputSystem

---
+++ proken-unity-sample/Assets/PlayerControl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// プロジェクトにパッケージ InputSystem をインストールしないと使えません
+// 新しいプロジェクトにインストールする手順：
+//     - 右上の検索から 「Package Manager」 を見つけて開くとウィンドウが出てくる
+//     - 「Packages : Unity Registry」の設定にしてリストから 「Input System」 を探す
+//     - それをインストールする
+//     - ダイアログが出てくるので Yes をクリックする
+//     - エディタが再起動する
+using UnityEngine.InputSystem;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
